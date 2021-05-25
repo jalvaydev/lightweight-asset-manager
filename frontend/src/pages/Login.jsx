962906f469b2c30412955abf4f232dd82cc1982d
Jesus Alvarez <jalvaydev@gmail.com>
2021-05-24 21:54:59 -0400

    added google sso to app

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,6 @@
 import { useOktaAuth } from "@okta/okta-react";
 import { useHistory } from "react-router-dom";
-import { Formik } from "formik";
-import Alert from "../components/Alert";
+import OktaSignInWidget from "../components/OktaSignInWidget";
 
 export default function Login() {
   const { oktaAuth, authState } = useOktaAuth();
@@ -12,13 +11,21 @@ export default function Login() {
     history.push("/dashboard");
   }
 
+  const onSuccess = (tokens) => {
+    oktaAuth.handleLoginRedirect(tokens);
+  };
+
+  const onError = (err) => {
+    console.log("error logging in", err);
+  };
+
   return (
     <div className="min-h-screen bg-white flex">
       <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
         <div className="mx-auto w-full max-w-sm lg:w-96">
           <div>
             <img
-              className="h-12 w-auto"
+              className="h-11 w-auto"
               src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
               alt="Workflow"
             />
@@ -27,156 +34,19 @@ export default function Login() {
             </h2>
             <p className="mt-2 text-sm text-gray-600">Proof of Concept Build</p>
           </div>
-
-          <div className="mt-8">
-            <div>
-              <div>
-                <p className="text-sm font-medium text-gray-700">
-                  Sign in with
-                </p>
-
-                <div className="mt-1 grid grid-cols-3">
-                  <div className="col-start-2 col-span-1">
-                    <a
-                      href="#"
-                      className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-                    >
-                      <span className="sr-only">Sign in with Google</span>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        preserveAspectRatio="xMidYMid"
-                        viewBox="0 0 256 262"
-                      >
-                        <path
-                          fill="#4285F4"
-                          d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
-                        ></path>
-                        <path
-                          fill="#34A853"
-                          d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"
-                        ></path>
-                        <path
-                          fill="#FBBC05"
-                          d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782"
-                        ></path>
-                        <path
-                          fill="#EB4335"
-                          d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"
-                        ></path>
-                      </svg>
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <div className="mt-6 relative">
-                <div
-                  className="absolute inset-0 flex items-center"
-                  aria-hidden="true"
-                >
-                  <div className="w-full border-t border-gray-300" />
-                </div>
-                <div className="relative flex justify-center text-sm">
-                  <span className="px-2 bg-white text-gray-500">
-                    Or continue with
-                  </span>
-                </div>
-              </div>
-            </div>
-
-            <div className="mt-6">
-              <Formik
-                initialValues={{ email: "", password: "" }}
-                validate={(values) => {
-                  const errors = {};
-                  if (!values.email) {
-                    errors.email = "Required";
-                  } else if (
-                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
-                      values.email
-                    )
-                  ) {
-                    errors.email = "Invalid email address";
-                  }
-                  return errors;
-                }}
-                onSubmit={async (values, { setSubmitting }) => {
-                  setSubmitting(true);
-                  try {
-                    const response = await oktaAuth.signInWithCredentials({
-                      username: values.email,
-                      password: values.password,
-                    });
-                    const sessionToken = response.sessionToken;
-                    oktaAuth.signInWithRedirect({
-                      sessionToken,
-                      prompt: "none",
-                    });
-                  } catch (err) {
-                    console.log(err);
-                  }
-                  setSubmitting(false);
-                }}
-              >
-                {({ errors, isSubmitting, handleChange, handleSubmit }) => (
-                  <form onSubmit={handleSubmit} className="space-y-6">
-                    <div>
-                      <label
-                        htmlFor="email"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Email address
-                      </label>
-                      {errors.email && (
-                        <Alert title="Error" text={errors.email} />
-                      )}
-                      <div className="mt-1">
-                        <input
-                          id="email"
-                          name="email"
-                          type="email"
-                          autoComplete="email"
-                          onChange={handleChange}
-                          className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        />
-                      </div>
-                    </div>
-
-                    <div className="space-y-1">
-                      <label
-                        htmlFor="password"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Password
-                      </label>
-                      <div className="mt-1">
-                        <input
-                          id="password"
-                          name="password"
-                          type="password"
-                          autoComplete="current-password"
-                          onChange={handleChange}
-                          required
-                          className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        />
-                      </div>
-                    </div>
-
-                    <div>
-                      <button
-                        type="submit"
-                        disabled={isSubmitting}
-                        className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                      >
-                        Sign in
-                      </button>
-                    </div>
-                  </form>
-                )}
-              </Formik>
-            </div>
+          <div>
+            <OktaSignInWidget
+              config={{
+                logo: "https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg",
+                baseUrl: "https://dev-41703573.okta.com",
+                clientId: "0oaopy53oHbY480ks5d6",
+                redirectUri: window.location.origin + "/login/callback",
+                idpDisplay: "PRIMARY",
+                idps: [{ type: "google", id: "0oasykoqcacZ9cEoB5d6" }],
+              }}
+              onSuccess={onSuccess}
+              onError={onError}
+            />
           </div>
         </div>
       </div>
